574425753fdf8c11ab9c142ef8fc1aa96891ddb8
N'Gouro dit Issa Sanogo <ngsanogo@gmail.com>
2020-05-04 18:13:30 +0200

    tests

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,9 @@
 # corpoaccident 0.0.0.9000
 
+* Added tests
+
 * Added a `NEWS.md` file to track changes to the package.
+
+* Added a `CODE_OF_CONDUCT.md` file
+
+* Added a `README.Rmd` file
--- tests/testthat/test-data-validity.R
+++ tests/testthat/test-data-validity.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Test nrow carac and lieu", {
+  expect_equal(base::nrow(carac) == nrow(lieu), TRUE)
+})
+
+test_that("Test years", {
+  expect_equal(base::all(base::unique(carac$an) %in% 5:18), TRUE)
+})
+
+test_that("Test months", {
+  expect_equal(base::all(base::unique(carac$mois) %in% 1:12), TRUE)
+})
+
+test_that("Test days", {
+  expect_equal(base::all(base::unique(carac$jour) %in% 1:31), TRUE)
+})
+
+test_that("Test hours", {
+  expect_equal(base::all(base::range(base::as.numeric(lubridate::hm(carac$hrmn))) == c(60, 86340)), TRUE)
+})
+
+test_that("Test birth year", {
+  expect_equal(base::max(usager$an_nais, na.rm = TRUE) == 2018, TRUE)
 })
